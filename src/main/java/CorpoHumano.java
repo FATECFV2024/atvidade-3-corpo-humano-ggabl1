3c392f7138be553a19286e4ecef2f826e831041f
Gabriella Sampaio Gomes de Castro <160081676+ggabl1@users.noreply.github.com>
2024-09-25 10:32:13 -0300

    Update CorpoHumano.java
    adicionando corpoHumano

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,39 @@
 package main.java;
 
 public class CorpoHumano {
+private Double massa;
+private Double volume;
+private Double densidade;
+
+public corpoHumano(Double massa, Double volume, Double densidade) {
+    this.massa = massa;
+    this.volume = volume;
+    this.densidade = densidade;
+}
+
+public Double getMassa() {
+    return massa;
+}
+
+public void setMassa(Double massa) {
+    this.massa = massa;
+}
+
+public Double getVolume() {
+    return volume;
+}
+
+public void setVolume(Double volume) {
+    this.volume = volume;
+}
+
+public Double getDensidade() {
+    return densidade;
+}
+
+public void setDensidade(Double densidade) {
+    this.densidade = densidade;
+}
+
 
 }
